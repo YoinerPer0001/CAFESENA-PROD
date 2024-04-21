38dd3d0450d16e73377bd95fa0c3185ef7021984
yoiner <oinerpertuz@gmail.com>
2024-04-21 17:35:27 -0500

    env email update

--- src/utils/Email.connection.js
+++ src/utils/Email.connection.js
@@ -1,14 +1,15 @@
 import nodemailer from "nodemailer";
+import 'dotenv/config.js'
 
 
 export const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
+    host: process.env.EMAHOST,
+    port: process.env.EMAPORT,
     secure: true,
     auth: {
       // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-      user: "senalearns@gmail.com",
-      pass: "gstu sooe mhwn puoj",
+      user: process.env.EMAUSER,
+      pass: process.env.EMAPASS,
     },
   });
 
